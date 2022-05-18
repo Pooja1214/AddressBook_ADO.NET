94b31be429d81bea3fddf53ec3660495e7f6bd1b
Niraj Suryavanshi <poojakadamlatur@gmail.com>
2022-05-18 17:15:42 +0530

    RetrieveDataBasedOnCity/State

---
+++ AddressBookRetrivelTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddressBookProgram;
+using System.Collections.Generic;
+
+namespace AddressBookRetrivelTest
+{
+    [TestClass]
+    public class AddressBookTest
+    {
+        OperationManagement operation;
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            operation = new OperationManagement();
+        }
+        //UC16-Retrive all data from database
+        [TestMethod]
+        public void RetrivingtheDataFromDataBaseTest()
+        {
+            int expected = 4;
+            List<ContactDetails> list = operation.RetrivingDataFromDataBase();
+            Assert.AreEqual(expected, list.Count);
+        }
+
+        //UC17-Update contact data from database
+        [TestMethod]
+        public void UpdateDataTest()
+        {
+            int expected = 1;
+            int actual = operation.updateContact(2, "Shanthi", 9845625362);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //UC19-Retrive the data based on city or state
+        [TestMethod]
+        public void StateCityRetrivalTest()
+        {
+            int expected = 4;
+            List<ContactDetails> actual = operation.RetrivingDataBasedOnStateCity("Tamil Nadu", "chennai");
+            Assert.AreEqual(expected, actual.Count);
+        }
+    }
+}
